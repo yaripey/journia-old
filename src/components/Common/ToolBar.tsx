3efbde82fd1beb967a2d4e4abc0d9c59d4921169
artem-trubin <artemtrubin.letters@gmail.com>
2024-01-02 14:47:52 +0200

    Reworked toolbar, removed tailwind there

--- src/components/Common/ToolBar.tsx
+++ src/components/Common/ToolBar.tsx
@@ -1,10 +1,10 @@
+import styled from "styled-components"
 import { PageName } from "../../types";
-import { Icon, IconName } from "./Icons";
 
-interface NavButton {
+interface ToolBarButton {
   name: string,
-  iconName: IconName,
   onClick: () => void,
+  iconFileName: string,
 }
 
 export const ToolBar = (
@@ -12,77 +12,72 @@ export const ToolBar = (
     setCurrentPage: (name: PageName) => void,
   }
 ) => {
-  const openEditorWithNewBlock = () => {
-    props.setCurrentPage("editor");
-  }
-
-  const openHomePage = () => {
-    props.setCurrentPage("home");
-  }
-
-  const defaultButtonStyles = [
-    "w-12",
-    "h-12",
-    "hover:bg-gray-300",
-  ];
-
-  const defaultIconStyles = [
-    "hover:stroke-black",
-    "p-3",
-    "stroke-white",
-  ];
-
-  const navButtons: Array<NavButton> = [
+  const buttons: ToolBarButton[] = [
     {
       name: "home",
-      iconName: "home",
-      onClick: openHomePage,
-    }
+      onClick: () => props.setCurrentPage("home"),
+      iconFileName: "src/assets/icons/home.png"
+    },
+    {
+      name: "create",
+      onClick: () => props.setCurrentPage("editor"),
+      iconFileName: "src/assets/icons/create.png"
+    },
   ];
 
-  return <div className="flex justify-between flex-col w-12 h-full bg-black">
-    <ul>
-      {
-        navButtons
-          .map((navButton, i) => (
-            <ToolBarButton
-              buttonStyles={defaultButtonStyles.join(" ")}
-              iconStyles={defaultIconStyles.join(" ")}
-              iconName={navButton.iconName}
-              onClick={navButton.onClick}
-              key={i}
-            />
-          ))
-      }
-    </ul>
+  const ToolBarContainer = styled.div`
+    display: flex;
+    justify-content: between;
+    flex-direction: column;
 
-    <ul>
-      <ToolBarButton
-        buttonStyles={defaultButtonStyles.join(" ")}
-        iconStyles={defaultIconStyles.join(" ")}
-        iconName="new"
-        onClick={openEditorWithNewBlock}
-      />
-    </ul>
-  </div>
+    width: 50px;
+    height: 100%;
+
+    color: white;
+  `;
+
+  return (
+    <ToolBarContainer>
+      {buttons.map(({ iconFileName, name, onClick }) =>
+        <ToolBarButton
+          iconFileName={iconFileName}
+          key={name}
+          onClick={onClick}
+        />
+      )}
+    </ToolBarContainer>
+  )
 }
 
 const ToolBarButton = (
   props: {
-    iconName: IconName,
-    iconStyles: string,
-    buttonStyles: string,
+    iconFileName: string,
     onClick: () => void,
   }
 ) => {
+  const NavBarButton = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 50px;
+    height: 50px;
+
+    background-color: white;
+
+    &:hover {
+      background-color: grey;
+    }
+  `;
+
+  const ButtonImg = styled.img`
+    width: 25px;
+    height: 25px;
+  `;
+
   return (
-    <li className="w-12 h-12">
-      <button className={props.buttonStyles} onClick={props.onClick}>
-        <Icon
-          iconName={props.iconName}
-          styles={props.iconStyles}
-        />
-      </button>
-    </li>
+    <NavBarButton onClick={props.onClick}>
+      <ButtonImg src={props.iconFileName} />
+    </NavBarButton>
   )
 }
