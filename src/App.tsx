1ebb9db4b33f50967a6d8badb7064f8b0004feaf
artem-trubin <artemtrubin.letters@gmail.com>
2024-01-21 20:54:58 +0200

    Another wipe

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
 import "./main.css"
 import testData from "./testData"
 
-import AppContainer from "./components/AppContainer"
-import Editor from "./components/sections/Editor"
-import SideBar from "./components/sections/SideBar"
-import { TimeLine } from "./components/sections/collections/Collection"
-
-import { useState } from "react"
-
-import { ContentBlock } from "./types"
-
-import { BlocksContext } from "./contexts/blocks"
-
 const App = () => {
-  const [blocks, setBlocks] = useState<ContentBlock[]>(testData)
 
   return (
-    <AppContainer>
-      <SideBar />
-      <BlocksContext.Provider value={{ blocks, setBlocks }}>
-        <TimeLine />
-      </BlocksContext.Provider>
-      <Editor />
-    </AppContainer>
+    <div>Test</div>
   )
 }
 export default App;
--- src/components/sections/collections/Collection.tsx
+++
@@ -1,7 +0,0 @@
-import TimeLine from "./TimeLine"
-
-// A collection is a part of the screen that represents a list of blocks. There are a lot of ways to represent these blocks, hense the separate folder for different representations.
-
-export {
-  TimeLine
-}
