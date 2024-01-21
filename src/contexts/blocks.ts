92bde00b4ce4550d7b622e453be12e920eb57234
artem-trubin <artemtrubin.letters@gmail.com>
2024-01-21 20:55:15 +0200

    Add zustand

--- src/contexts/blocks.ts
+++
@@ -1,4 +0,0 @@
-import { Dispatch, createContext } from "react"
-import { ContentBlock } from "../types"
-
-export const BlocksContext = createContext<{ blocks: ContentBlock[], setBlocks: Dispatch<ContentBlock[]> }>({ blocks: [], setBlocks: () => { } })
