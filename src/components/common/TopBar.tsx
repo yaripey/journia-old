9c95be66e30ff98880b646367015565078f93571
artem-trubin <artemtrubin.letters@gmail.com>
2024-01-24 19:29:41 +0200

    Plus icon now redirects to new block type selector screen

--- src/components/common/TopBar.tsx
+++ src/components/common/TopBar.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components"
 import TopBarButton from "./TopBarButton"
 import React from "react"
+import useEditorStore from "../../store/editorStore"
+import useScreenStore from "../../store/screenStore"
 
 const TopBarContainer = styled.div`
   width: 100%;
@@ -11,15 +13,19 @@ const TopBarContainer = styled.div`
 `
 
 const TopBar: React.FC = () => {
-  const createNew = () => {
-    console.log("Creating new note")
+  const clearEditingBlock = useEditorStore(store => store.clearEditingBlock)
+  const setCurrentScreen = useScreenStore(store => store.setCurrentScreen)
+
+  const createNewBlock = () => {
+    clearEditingBlock()
+    setCurrentScreen("blockSelector")
   }
 
   return (
     <TopBarContainer>
       <div></div>
       <div>
-        <TopBarButton icon="plus" onClick={createNew} />
+        <TopBarButton icon="plus" onClick={createNewBlock} />
       </div>
     </TopBarContainer>
   )
